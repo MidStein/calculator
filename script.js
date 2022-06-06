4bd5ad1949eb4314a8f84c2e4385f71a74a180ac
MidStein <deepakchauhan19feb@gmail.com>
2022-06-06 09:03:10 +0530

    prepared operate function

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+function operate(operator, num1, num2) {
+    if (operator === '+') {
+        add(num1, num2);
+    } else if (operator === '-') {
+        subtract(num1, num2);
+    } else if (operator === '*') {
+        multiply(num1, num2);
+    } else {
+        divide(num1, num2);
+    }
+}

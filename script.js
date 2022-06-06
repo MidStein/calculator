d1254af3ab0728dc028d15f99e089572c56b1d78
MidStein <deepakchauhan19feb@gmail.com>
2022-06-06 14:06:47 +0530

    calculator is ready

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 function subtract(num1, num2) {
     return num1 - num2;
@@ -12,12 +12,36 @@ function divide(num1, num2) {
 }
 function operate(operator, num1, num2) {
     if (operator === '+') {
-        add(num1, num2);
+        return add(num1, num2);
     } else if (operator === '-') {
-        subtract(num1, num2);
+        return subtract(num1, num2);
     } else if (operator === '*') {
-        multiply(num1, num2);
+        return multiply(num1, num2);
     } else {
-        divide(num1, num2);
+        return divide(num1, num2);
     }
 }
+function equalsClicked() {
+    display.textContent = operate(operator, operand1, operand2);
+}
+function numberClicked() {
+    display.textContent += this.textContent;
+    if (operatorEntered) {
+        operand2 += this.textContent;
+    }
+}
+function operationClicked() {
+    operand1 = display.textContent;
+    operator = this.textContent;
+    display.textContent += " " + this.textContent + " ";
+    operatorEntered = true;
+}
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.number');
+const operations = document.querySelectorAll('.operation');
+const equals = document.querySelector('.equals');
+let operand1, operator, operand2 = '', operatorEntered = false;
+numbers.forEach(number => number.addEventListener('click', numberClicked));
+operations.forEach(operation => operation.addEventListener('click', operationClicked));
+equals.addEventListener('click', equalsClicked);
+display.textContent = '';
